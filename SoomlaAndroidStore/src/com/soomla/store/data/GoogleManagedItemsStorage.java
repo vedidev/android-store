635d4dac89a2e7112c9a17b77b75e521827b49de
refaelos <refaeldakar@gmail.com>
2012-10-18 15:33:06 +0200

    - supporting MANAGED items. just define them in your IStoreAssets and they'll show up in StoreInfo for you. You can use StoreController to purchase a MANAGED item.

---
+++ SoomlaAndroidStore/src/com/soomla/store/data/GoogleManagedItemsStorage.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2012 Soomla Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.soomla.store.data;
+
+import android.database.Cursor;
+import android.util.Log;
+import com.soomla.billing.util.AESObfuscator;
+import com.soomla.store.StoreConfig;
+import com.soomla.store.domain.data.GoogleMarketItem;
+import com.soomla.store.domain.data.VirtualCurrency;
+
+/**
+ * This class provide basic storage operations on Google Play's MANAGED items.
+ */
+public class GoogleManagedItemsStorage {
+
+    /** Constructor
+     *
+     */
+    public GoogleManagedItemsStorage() {
+    }
+
+    /** Public functions **/
+
+    /**
+     * Figure out if the given MANAGED {@link GoogleMarketItem} exists.
+     * @param googleMarketItem the required MANAGED {@link GoogleMarketItem}.
+     * @return whether the given item exists or not.
+     */
+    public boolean googleManagedItemExists(GoogleMarketItem googleMarketItem){
+
+        if (StoreConfig.debug){
+            Log.d(TAG, "trying to figure out if the given MANAGED item exists.");
+        }
+
+        String productId = googleMarketItem.getProductId();
+        if (StorageManager.getInstance().getObfuscator() != null){
+            productId = StorageManager.getInstance().getObfuscator().obfuscateString(productId);
+        }
+        Cursor cursor = StorageManager.getInstance().getDatabase().getGoogleManagedItem(productId);
+
+        if (cursor == null) {
+            return false;
+        }
+
+        int productIdCol = cursor.getColumnIndexOrThrow(
+                    StoreDatabase.GOOGLE_MANAGED_ITEMS_COLUMN_PRODUCT_ID);
+        if (cursor.moveToNext()) {
+            if (StoreConfig.debug){
+                Log.d(TAG, "the google managed item exists: " + googleMarketItem.getProductId());
+            }
+
+            cursor.close();
+            return true;
+        }
+
+        cursor.close();
+        return false;
+    }
+
+    /**
+     * Adds the given google MANAGED item to the storage.
+     * @param googleMarketItem is the required google MANAGED item.
+     */
+    public void add(GoogleMarketItem googleMarketItem){
+        if (StoreConfig.debug){
+            Log.d(TAG, "adding " + googleMarketItem.getProductId());
+        }
+
+        String productId = googleMarketItem.getProductId();
+        if (StorageManager.getInstance().getObfuscator() != null){
+            productId = StorageManager.getInstance().getObfuscator().obfuscateString(productId);
+        }
+        StorageManager.getInstance().getDatabase().setGoogleManagedItem(productId, true);
+    }
+
+    /**
+     * Removes the given google MANAGED item from the storage.
+     * @param googleMarketItem is the required google MANAGED item.
+     */
+    public void remove(GoogleMarketItem googleMarketItem){
+        if (StoreConfig.debug){
+            Log.d(TAG, "removing " + googleMarketItem.getProductId());
+        }
+
+        String productId = googleMarketItem.getProductId();
+        if (StorageManager.getInstance().getObfuscator() != null){
+            productId = StorageManager.getInstance().getObfuscator().obfuscateString(productId);
+        }
+        StorageManager.getInstance().getDatabase().setGoogleManagedItem(productId, false);
+    }
+
+    /** Private members **/
+
+    private static final String TAG = "SOOMLA GoogleManagedItemsStorage";
+}
