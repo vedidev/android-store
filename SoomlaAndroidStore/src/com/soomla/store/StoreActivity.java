764639e8a0b48f0c6ffde7bd65bc2e188ae73b16
Ron Mordechai <ronmrdechai@gmail.com>
2013-11-25 14:09:18 +0200

    Updated class to use new design, purchasing is done through here now

--- SoomlaAndroidStore/src/com/soomla/store/StoreActivity.java
+++ SoomlaAndroidStore/src/com/soomla/store/StoreActivity.java
@@ -4,40 +4,39 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.soomla.store.events.UnexpectedStoreErrorEvent;
+import com.soomla.store.exceptions.VirtualItemNotFoundException;
+
 public class StoreActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        String productId = intent.getStringExtra(StoreController.PROD_ID);
+        String payload = intent.getStringExtra(StoreController.EXTRA_DATA);
+
+        try {
+            StoreController.getInstance().buyWithGooglePlayInner(this, productId, payload);
+        } catch (IllegalStateException e) {
+            StoreUtils.LogError(TAG, "Error purchasing item " + e.getMessage());
+            BusProvider.getInstance().post(new UnexpectedStoreErrorEvent());
+            finish();
+        } catch (VirtualItemNotFoundException e) {
+            StoreUtils.LogError(TAG, "Couldn't find a purchasable item with productId: " + productId);
+            BusProvider.getInstance().post(new UnexpectedStoreErrorEvent());
+            finish();
+        }
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (!StoreController.getInstance().handleActivityResult(requestCode, resultCode, data)) {
             super.onActivityResult(requestCode, resultCode, data);
+        } else {
+            finish();
         }
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        StoreController.getInstance().setCurrentActivity(this);
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        StoreController.getInstance().setCurrentActivity(this);
-    }
-    @Override
-    protected void onPause() {
-        clearReferences();
-        super.onPause();
-    }
-    @Override
-    protected void onDestroy() {
-        clearReferences();
-        super.onDestroy();
-    }
-
-    /* make sure we don't reference a dead activity */
-    private void clearReferences() {
-        Activity activity = StoreController.getInstance().getCurrentActivity();
-        if (activity != null && activity.equals(this))
-            StoreController.getInstance().setCurrentActivity(null);
-    }
+    private static String TAG = "SOOMLA StoreActivity";
 }
